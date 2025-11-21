a3bbce11ba4199745ba702c9faa6004c4527747c
emce <emcebehrens@gmail.com>
2025-11-21 18:12:23 -0300

    Light and dark mode working

--- README.md
+++ README.md
@@ -4,3 +4,10 @@
 Created with:
 
 npm create vite@latest PROJECT_NAME --template vue-ts
+
+
+Tailwind
+npm install tailwindcss @tailwindcss/vite
+
+Then add tailwind plugin in vite.config.ts
+Add @import "tailwindcss"; in style.css
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -7,23 +7,25 @@ const count = ref(0)
 </script>
 
 <template>
-  <h1>{{ msg }}</h1>
+  <h1 class="mb-6 text-primary dark:text-primary-dark">{{ msg }}</h1>
 
   <div class="card">
-    <button type="button" @click="count++">count is {{ count }}</button>
-    <p>
+    <button type="button" @click="count++" class="text-primary dark:text-primary-dark">
+      count is {{ count }}
+    </button>
+    <p class="text-primary dark:text-primary-dark">
       Edit
       <code>components/HelloWorld.vue</code> to test HMR
     </p>
   </div>
 
-  <p>
+  <p class="text-primary dark:text-primary-dark">
     Check out
     <a href="https://vuejs.org/guide/quick-start.html#local" target="_blank"
       >create-vue</a
     >, the official Vue + Vite starter
   </p>
-  <p>
+  <p class="text-primary dark:text-primary-dark">
     Learn more about IDE Support for Vue in the
     <a
       href="https://vuejs.org/guide/scaling-up/tooling.html#ide-support"
@@ -31,11 +33,8 @@ const count = ref(0)
       >Vue Docs Scaling up Guide</a
     >.
   </p>
-  <p class="read-the-docs">Click on the Vite and Vue logos to learn more</p>
 </template>
 
 <style scoped>
-.read-the-docs {
-  color: #888;
-}
+
 </style>
